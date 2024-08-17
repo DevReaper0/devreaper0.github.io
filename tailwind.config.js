cad71539edaf8ed39b99aa89ae3f312d07e3a6ae
DevReaper0 <devreaper0@gmail.com>
2024-08-17 20:37:44 +0000

    Added a page fade and changed the dark mode theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,6 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: [
-      "light",
-      catppuccin("mocha"),
-    ],
+    themes: ["light", "dark", "sunset", catppuccin("mocha")],
   },
 };
