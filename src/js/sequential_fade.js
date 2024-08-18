2c6ea9b21620cdde04c68a6f7c29a6f11842ae61
DevReaper0 <devreaper0@gmail.com>
2024-08-18 00:06:36 +0000

    Fixed an issue where sequential fading had extra delay.

--- src/js/sequential_fade.js
+++ src/js/sequential_fade.js
@@ -1,11 +1,15 @@
+const fadeTime = 3000;
+
 const fadeElementSets = [];
 
-let i = 1;
+let counter = 1;
 while (true) {
-  const elements = [...document.getElementsByClassName(`sequential-fade-${i}`)];
+  const elements = [
+    ...document.getElementsByClassName(`sequential-fade-${counter}`),
+  ];
   if (elements.length === 0) break;
   fadeElementSets.push(elements);
-  i++;
+  counter++;
 }
 
 fadeElementSets.forEach((fadeElementSet) => {
@@ -14,29 +18,31 @@ fadeElementSets.forEach((fadeElementSet) => {
   });
 });
 
-for (let i = 0; i < fadeElementSets.length - 1; i++) {
-  const currentElementSet = fadeElementSets[i];
-  const nextElementSet = fadeElementSets[i + 1];
-
-  currentElementSet[0].addEventListener("animationend", (e) => {
-    if (e.animationName !== "fadeIn") return;
-    nextElementSet.forEach((el) => {
-      el.style.animation = "fadeIn 3s forwards";
-    });
-  });
-}
-
 const typewriter = document.getElementsByClassName("initial-typewriter")[0];
 if (typewriter) {
   typewriter.addEventListener("typingFinish", (e) => {
-    fadeElementSets[0].forEach((el) => {
-      el.style.animation = "fadeIn 3s forwards";
-    });
+    for (let i = 0; i < fadeElementSets.length; i++) {
+      setTimeout(
+        () => {
+          fadeElementSets[i].forEach((el) => {
+            el.style.animation = `fadeIn ${fadeTime}ms forwards`;
+          });
+        },
+        fadeTime * 0.5 * i
+      );
+    }
   });
 } else {
-  fadeElementSets[0].forEach((el) => {
-    el.style.animation = "fadeIn 3s forwards";
-  });
+  for (let i = 0; i < fadeElementSets.length; i++) {
+    setTimeout(
+      () => {
+        fadeElementSets[i].forEach((el) => {
+          el.style.animation = `fadeIn ${fadeTime}ms forwards`;
+        });
+      },
+      fadeTime * 0.5 * i
+    );
+  }
 }
 
 function finalFadeEnd() {
@@ -47,10 +53,13 @@ function finalFadeEnd() {
 }
 
 if (fadeElementSets.length > 0) {
-  fadeElementSets[fadeElementSets.length - 1][0].addEventListener("animationend", (e) => {
-    if (e.animationName !== "fadeIn") return;
-    finalFadeEnd();
-  });
+  fadeElementSets[fadeElementSets.length - 1][0].addEventListener(
+    "animationend",
+    (e) => {
+      if (e.animationName !== "fadeIn") return;
+      finalFadeEnd();
+    }
+  );
 } else if (typewriter) {
   typewriter.addEventListener("typingFinish", (e) => {
     finalFadeEnd();
